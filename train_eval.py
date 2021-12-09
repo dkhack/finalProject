210c4ea275485ff75ba5aef5730bb8d495117290
Dipin Khati <dipinkhati@gmail.com>
2021-12-08 22:12:30 -0500

    Removed unused function, updated runs and epochs

--- train_eval.py
+++ train_eval.py
@@ -48,35 +48,6 @@ def random_planetoid_splits(data, num_classes, lcc_mask):
 
     return data
 
-def random_coauthor_amazon_splits(data, num_classes, lcc_mask):
-    # Set random coauthor/co-purchase splits:
-    # * 20 * num_classes labels for training
-    # * 30 * num_classes labels for validation
-    # rest labels for testing
-
-    indices = []
-    if lcc_mask is not None:
-        for i in range(num_classes):
-            index = (data.y[lcc_mask] == i).nonzero().view(-1)
-            index = index[torch.randperm(index.size(0))]
-            indices.append(index)
-    else:
-        for i in range(num_classes):
-            index = (data.y == i).nonzero().view(-1)
-            index = index[torch.randperm(index.size(0))]
-            indices.append(index)
-
-    train_index = torch.cat([i[:20] for i in indices], dim=0)
-    val_index = torch.cat([i[20:50] for i in indices], dim=0)
-
-    rest_index = torch.cat([i[50:] for i in indices], dim=0)
-    rest_index = rest_index[torch.randperm(rest_index.size(0))]
-
-    data.train_mask = index_to_mask(train_index, size=data.num_nodes)
-    data.val_mask = index_to_mask(val_index, size=data.num_nodes)
-    data.test_mask = index_to_mask(rest_index, size=data.num_nodes)
-
-    return data
 
 
 def run(dataset, model, runs, epochs, lr, weight_decay, early_stopping,
